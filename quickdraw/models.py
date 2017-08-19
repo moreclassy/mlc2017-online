50a0b46a71b64a06fa6480fdee46e9b91fb3c885
moreclassy <kimbyungha@kims-MacBook-Air.local>
2017-08-19 20:18:01 +0900

    adsfhfoai

--- quickdraw/models.py
+++ quickdraw/models.py
@@ -30,9 +30,9 @@ class Convolution(BaseModel):
   def create_model(self, model_input, num_classes=10, l2_penalty=1e-8, **unused_params):
     net = slim.repeat(model_input, 2, slim.conv2d, 64, [3, 3], scope='conv1')
     net = slim.max_pool2d(net, [2, 2], scope='pool1')
-    net = slim.repeat(net, 2, slim.conv2d, 128, [3, 3], scope='conv2')
+    """net = slim.repeat(net, 2, slim.conv2d, 128, [3, 3], scope='conv2')
     net = slim.max_pool2d(net, [2, 2], scope='pool2')
-    """net = slim.repeat(net, 3, slim.conv2d, 256, [3, 3], scope='conv3')
+    net = slim.repeat(net, 3, slim.conv2d, 256, [3, 3], scope='conv3')
     net = slim.max_pool2d(net, [2, 2], scope='pool3')
     net = slim.repeat(net, 3, slim.conv2d, 512, [3, 3], scope='conv4')
     net = slim.max_pool2d(net, [2, 2], scope='pool4')
