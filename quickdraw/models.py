1cc801c437c60aa8886d12901c6d2fd153006ebf
moreclassy <kimbyungha@kims-MacBook-Air.local>
2017-08-19 21:08:10 +0900

    ahodfi

--- quickdraw/models.py
+++ quickdraw/models.py
@@ -29,12 +29,21 @@ class BaseModel(object):
 class Convolution(BaseModel):
     
   def create_model(self, model_input, num_classes=10, l2_penalty=1e-8, **unused_params):
-    net = slim.conv2d(model_input, 20, [5,5], scope='conv1')
-    net = slim.max_pool2d(net, [2,2], scope='pool1')
-    net = slim.conv2d(net, 50, [5,5], scope='conv2')
-    net = slim.max_pool2d(net, [2,2], scope='pool2')
-    net = slim.flatten(net, scope='flatten3')
-    net = slim.fully_connected(net, 500, scope='fc4')
+    net = slim.repeat(model_input, 2, slim.conv2d, 64, [3, 3])
+    net = slim.max_pool2d(net, [2, 2])
+    net = slim.repeat(net, 2, slim.conv2d, 128, [3, 3])
+    net = slim.max_pool2d(net, [2, 2])
+    net = slim.repeat(net, 3, slim.conv2d, 256, [3, 3])
+    net = slim.max_pool2d(net, [2, 2])
+    net = slim.repeat(net, 3, slim.conv2d, 512, [3, 3])
+    net = slim.max_pool2d(net, [2, 2], scope=)
+    net = slim.repeat(net, 3, slim.conv2d, 512, [3, 3])
+    net = slim.max_pool2d(net, [2, 2])
+    net = slim.fully_connected(net, 4096)
+    net = slim.dropout(net, 0.5)
+    net = slim.fully_connected(net, 4096)
+    net = slim.dropout(net, 0.5)
+    net = slim.flatten(net)
     output = slim.fully_connected(
         net, num_classes, activation_fn=None,
         weights_regularizer=slim.l2_regularizer(l2_penalty))
