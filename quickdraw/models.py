f1d38305ad1d4dacd9831cf5fec7d906303fa2c8
moreclassy <kimbyungha@kims-MacBook-Air.local>
2017-08-20 12:48:07 +0900

    conadfa

--- quickdraw/models.py
+++ quickdraw/models.py
@@ -83,16 +83,35 @@ class Convolution3(BaseModel):
 class Convolution4(BaseModel):
     
     def create_model(self, model_input, num_classes=10, l2_penalty=1e-8, **unused_params):
-        net = slim.repeat(model_input, 2, slim.conv2d, 64, [3, 3])
-        net = slim.max_pool2d(net, [4, 4])
-        net = slim.repeat(net, 3, slim.conv2d, 32, [3, 3])
-        net = slim.max_pool2d(net, [2, 2])
-        net = slim.fully_connected(net, 256)
+        net = tf.layers.conv2d(
+            inputs=model_input,
+            filters=32,
+            kernel_size=[5, 5],
+            padding="same",
+            activation=tf.nn.relu)
+        net = tf.layers.max_pooling2d(inputs=net, pool_size=[2, 2], strides=2)
+        net = tf.layers.conv2d(
+            inputs=net,
+            filters=64,
+            kernel_size=[5, 5],
+            padding="same",
+            activation=tf.nn.relu)
+        net = tf.layers.max_pooling2d(inputs=net, pool_size=[2, 2], strides=2)
+        net = slim.fully_connected(net, 100)
+        net = slim.dropout(net, 0.5)
+        net = tf.layers.conv2d(
+           inputs=net,
+           filters=64,
+           kernel_size=[5, 5],
+           padding="same",
+           activation=tf.nn.relu)
+        net = tf.layers.max_pooling2d(inputs=net, pool_size=[2, 2], strides=2)
+        net = slim.fully_connected(net, 100)
         net = slim.dropout(net, 0.5)
         net = slim.flatten(net)
         output = slim.fully_connected(
-          net, num_classes, activation_fn=None,
-          weights_regularizer=slim.l2_regularizer(l2_penalty))
+            net, num_classes, activation_fn=None,
+            weights_regularizer=slim.l2_regularizer(l2_penalty))
         return {"predictions": output}
 
 class LogisticModel(BaseModel):
