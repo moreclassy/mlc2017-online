a2db62dff68feaa66901ce6ea9cdfb8c9d02c548
moreclassy <kimbyungha@kims-MacBook-Air.local>
2017-08-20 22:50:31 +0900

    adfh

--- quickdraw/models.py
+++ quickdraw/models.py
@@ -193,7 +193,7 @@ class InceptionModel(BaseModel):
 
 class InceptionResNetModel(BaseModel):
     def create_model(self, model_input, num_classes=10, is_training=True, **unused_params):
-        model_input = tf.image.resize_images(model_input, [224, 224])
+        model_input = tf.image.resize_images(model_input, [5, 299, 299, 1])
         output = inception_resnet_v2.inception_resnet_v2(model_input, num_classes=num_classes)[0]
         output = tf.reshape(output, [-1, num_classes])
         output = tf.nn.softmax(output)
